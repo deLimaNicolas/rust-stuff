cab1a6f4ca1bbfad0c66f11f711d7797c954d544
deLimaNicolas <teixlima@gmail.com>
2026-02-20 21:38:05 -0300

    varaibles

--- variables/target/debug/deps/variables-cd5644b051c6db12.d
+++
@@ -1,5 +0,0 @@
-/Users/nico/Documents/hackerboy/personal/rust-stuff/variables/target/debug/deps/variables-cd5644b051c6db12.d: src/main.rs
-
-/Users/nico/Documents/hackerboy/personal/rust-stuff/variables/target/debug/deps/variables-cd5644b051c6db12: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/nico/Documents/hackerboy/personal/rust-stuff/variables/target/debug/variables: /Users/nico/Documents/hackerboy/personal/rust-stuff/variables/src/main.rs
