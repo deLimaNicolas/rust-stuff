8a4a9c857bd9d685cb262c3a882e208c80b07927
deLimaNicolas <teixlima@gmail.com>
2026-03-03 19:57:32 -0300

    slices

--- ownership/slices/target/debug/deps/slices-02a049f92d681d32.d
+++
@@ -1,5 +0,0 @@
-/Users/nico/Documents/hackerboy/personal/rust-stuff/ownership/slices/target/debug/deps/slices-02a049f92d681d32.d: src/main.rs
-
-/Users/nico/Documents/hackerboy/personal/rust-stuff/ownership/slices/target/debug/deps/slices-02a049f92d681d32: src/main.rs
-
-src/main.rs:
--- ownership/slices/target/debug/slices.d
+++
@@ -1,1 +0,0 @@
-/Users/nico/Documents/hackerboy/personal/rust-stuff/ownership/slices/target/debug/slices: /Users/nico/Documents/hackerboy/personal/rust-stuff/ownership/slices/src/main.rs
