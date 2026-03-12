b659be69dc781e5267cd4f358377c8cfeb09eacc
deLimaNicolas <teixlima@gmail.com>
2026-03-12 00:03:39 -0300

    use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Result;
+use std::io::Result as DiffResult;
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -39,10 +41,13 @@ mod back_of_house {
 
 fn deliver_order() {}
 
+use front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
+    hosting::add_to_waitlist();
 
     //meal.seasonal_fruit = String::from("blueberries");
 }
