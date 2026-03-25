07608772210cb6207a0435755ef85059787f4334
deLimaNicolas <teixlima@gmail.com>
2026-03-25 17:31:57 -0300

    gen

---
+++ generics/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let number_list = vec![3, 4, 99, 1, 3, 0];
+    let number_list2 = vec![300, 4, 99, 1, 3, 0];
+    let largest = get_largest(&number_list);
+    let largest2 = get_largest(&number_list2);
+    println!("Largest is {largest}");
+    println!("Largest is {largest2}");
+}
+
+fn get_largest(list: &Vec<usize>) -> usize {
+    let mut largest = list[0];
+
+    for num in list {
+        if *num > largest {
+            largest = *num;
+        }
+    }
+
+    largest
+}
